2a04ff8b536e6aed707c84cfeed2aa4e20bcce15
CoderMCH <160289936+CoderMCH@users.noreply.github.com>
2024-02-27 20:04:05 -0500

    step3
    add loadList() and loadDetails()

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,6 @@
-const eType = {
-    grass: [0, "grass"],
-    posion: [1, "posion"],
-    fire: [2, "fire"],
-    ice: [3, "ice"],
-    flying: [4, "flying"],
-    psychic: [5, "psychic"],
-    water: [6, "water"],
-    ground: [7, "ground"],
-    rock: [8, "rock"],
-    electric: [9, "electric"]
-};
-const eCategory = {
-    seed: [0, "seed"],
-    lizard: [1, "lizard"],
-    flame: [2, "flame"]
-};
-
 let repoPokemon = (function() {
     let aPokemonList = [];
-    const pPokemonTemplate = {
-        id: 1,
-        name: "Bulbasaur",
-        evolution: 2,
-        degeneration: null,
-        types: [eType.grass, eType.posion],
-        category: eCategory.seed,
-        state: {
-            HP: 45,
-            ATK: 49,
-            DEF: 49,
-            SPD: 45,
-            SP_ATK: 65,
-            SP_DEF: 65
-        },
-        profile: {
-            height: 0.7,
-            weight: 6.9,
-            maleRatio: 0.875,
-        }
-    }
+    let apiUrl = "https://pokeapi.co/api/v2/pokemon/";
 
     function add(pPokemon) {
         aPokemonList.push(pPokemon);
@@ -64,21 +26,46 @@ let repoPokemon = (function() {
     }
     function addListItem(pPokemon) {;
         let button = document.createElement("button");
-        button.classList.add("pekomon-list-button");
+        button.classList.add("pokemon-list-button");
         button.innerText = pPokemon.name;
         addButtonEvenListener(button, pPokemon);
         
         let ulPekomonList = document.querySelector("ul.pokemon-list");
         let liPekomon = document.createElement("li");
-        liPekomon.classList.add("pekomon-list-item");
+        liPekomon.classList.add("pokemon-list-item");
         liPekomon.appendChild(button);
         ulPekomonList.appendChild(liPekomon);
     }
-    function showDetail(pPokemon) {
-        console.log(pPokemon);
+    function showDetails(pPokemon) {
+        let p = document.querySelector(".pokemon-details");
+        p.innerText = pPokemon.name;
     }
     function addButtonEvenListener(pButton, pPokemon) {
-        pButton.addEventListener("click", ev => showDetail(pPokemon));
+        pButton.addEventListener("click", ev => showDetails(pPokemon));
+    }
+
+    function loadList() {
+        fetch(apiUrl, {method: "GET"}).then(res => {
+            return res.json();
+        }).then(json => {
+            json.results.forEach(pokemon => {
+                add({
+                    name: pokemon.name,
+                    detailsUrl: pokemon.url
+                })
+            })
+        }).catch(err => console.error(err))
+    }
+    function loadDetails(pPokemon) {
+        fetch(pPokemon.detailsUrl, {method: "GET"}).then(res => {
+            return res.json();
+        }).then(json => {
+            console.log(json)
+            pPokemon.id = json.id;
+            pPokemon.types = json.types;
+            pPokemon.sprite = json.sprite;
+            pPokemon.height = json.height;
+        }).catch(err => console.error(err))
     }
 
     return {
@@ -88,144 +75,13 @@ let repoPokemon = (function() {
         addv: addv,
         filterByName: filterByName,
         addListItem: addListItem,
-        showDetail: showDetail
+        showDetail: showDetails,
+        loadList: loadList,
+        loadDetails: loadDetails
     };
 })();
 
-// reference to https://pokedex.org/
-let pBulbasaur = {
-    id: 1,
-    name: "Bulbasaur",
-    evolution: 2,
-    degeneration: null,
-    types: [eType.grass, eType.posion],
-    category: eCategory.seed,
-    state: {
-        HP: 45,
-        ATK: 49,
-        DEF: 49,
-        SPD: 45,
-        SP_ATK: 65,
-        SP_DEF: 65
-    },
-    profile: {
-        height: 0.7,
-        weight: 6.9,
-        maleRatio: 0.875,
-    }
-};
-let pIvysaur = {
-    id: 2,
-    name: "Ivysaur",
-    evolution: 3,
-    degeneration: 1,
-    types: [eType.grass, eType.posion],
-    category: eCategory.seed,
-    state: {
-        HP: 60,
-        ATK: 62,
-        DEF: 63,
-        SPD: 60,
-        SP_ATK: 80,
-        SP_DEF: 80
-    },
-    profile: {
-        height: 1,
-        weight: 13,
-        maleRatio: 0.875,
-    }
-};
-let pVenusaur = {
-    id: 3,
-    name: "Venusaur",
-    evolution: null,
-    degeneration: 2,
-    types: [eType.grass, eType.posion],
-    category: eCategory.seed,
-    state: {
-        HP: 80,
-        ATK: 82,
-        DEF: 83,
-        SPD: 80,
-        SP_ATK: 100,
-        SP_DEF: 100
-    },
-    profile: {
-        height: 2,
-        weight: 100,
-        maleRatio: 0.875,
-    }
-};
-let pCharmander = {
-    id: 4,
-    name: "Charmander",
-    evolution: 5,
-    degeneration: null,
-    types: eType.fire,
-    category: eCategory.lizard,
-    state: {
-        HP: 39,
-        ATK: 52,
-        DEF: 43,
-        SPD: 65,
-        SP_ATK: 60,
-        SP_DEF: 50
-    },
-    profile: {
-        height: 0.6,
-        weight: 8.5,
-        maleRatio: 0.875,
-    }
-};
-let pCharmeleon = {
-    id: 5,
-    name: "Charmeleon",
-    evolution: 6,
-    degeneration: 4,
-    types: eType.fire,
-    category: eCategory.flame,
-    state: {
-        HP: 58,
-        ATK: 64,
-        DEF: 58,
-        SPD: 80,
-        SP_ATK: 80,
-        SP_DEF: 65
-    },
-    profile: {
-        height: 1.1,
-        weight: 19,
-        maleRatio: 0.875,
-    }
-};
-let pCharizard = {
-    id: 6,
-    name: "Charizard",
-    evolution: null,
-    degeneration: 5,
-    types: [eType.fire, eType.flying],
-    category: eCategory.flame,
-    state: {
-        HP: 78,
-        ATK: 84,
-        DEF: 78,
-        SPD: 100,
-        SP_ATK: 109,
-        SP_DEF: 85
-    },
-    profile: {
-        height: 1.7,
-        weight: 90.5,
-        maleRatio: 0.875,
-    }
-};
-
-repoPokemon.addv(pBulbasaur);
-repoPokemon.addv(pIvysaur);
-repoPokemon.addv(pVenusaur);
-repoPokemon.addv(pCharmander);
-repoPokemon.addv(pCharmeleon);
-repoPokemon.addv(pCharizard);
+repoPokemon.loadList();
 
 repoPokemon.getAll().forEach(pPokemon => {
     repoPokemon.addListItem(pPokemon);
